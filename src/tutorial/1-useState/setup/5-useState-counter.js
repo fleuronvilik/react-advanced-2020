c18a9fe86064a3d579fe7b1e2d6cb59cc79d0c41
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-03 21:41:39 +0100

    Add a Complex Counter
    Both counters share the same state
    The complex one use the functional state update form
    To get rid of a side effect of `setTimeout`

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -4,18 +4,35 @@ const UseStateCounter = () => {
   const [count, setCount] = useState(0);
 
   const reset = () => setCount(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // console.log(count)
+      // setCount(count+1)
+      setCount(prevCount => prevCount + 1)
+    }, 2000);
+  }
+  
   return (
-    <>
-      <h2>regular counter</h2>
-      <h1>{count}</h1>
-      <button className="btn" onClick={() => setCount(count-1)}>
-        decrease
-      </button>
-      <button className="btn" onClick={reset}>reset</button>
-      <button className="btn" onClick={() => setCount(count+1)}>
-        increase
-      </button>
-    </>
+    <React.Fragment>
+      <section>
+        <h2>regular counter</h2>
+        <h1>{count}</h1>
+        <button className="btn" onClick={() => setCount(count-1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={reset}>reset</button>
+        <button className="btn" onClick={() => setCount(count+1)}>
+          increase
+        </button>
+      </section>
+      <section style={{margin: "4rem 0"}}>
+        <h2>complex counter</h2>
+        <h1>{count}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase
+        </button>
+      </section>
+    </React.Fragment>
   );
 };
 
