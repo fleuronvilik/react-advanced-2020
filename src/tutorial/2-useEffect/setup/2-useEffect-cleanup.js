f95a993241e1ec3fa3d6d4c991f70281c546748a
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-06 23:30:19 +0100

    Complete useEffect CleanUp

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
-// cleanup function
+// cleanup function[Complete]
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [width, setWidth] = useState(window.innerWidth);
+  //console.log("RENDER", width)
+
+  function updateWidth() {
+    setWidth(window.innerWidth);
+    //console.log("render")
+  }
+
+  useEffect(() => {
+    //console.log("useEffect")
+    window.addEventListener("resize", updateWidth);
+    return () => {
+      //console.log("cleanup")
+      window.removeEventListener("resize", updateWidth);
+    }
+  });
+
+  return (
+    <React.Fragment>
+      <h2>Width: {width}px</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
