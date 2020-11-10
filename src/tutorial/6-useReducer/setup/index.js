0a773d694bd4ea213871541805bdfe0fef8b5adb
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-11 00:27:31 +0100

    Complete useState Setup before useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,47 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (name) {
+      setPeople([...people, {
+        id: new Date().getTime().toString(),
+        name: name
+      }])
+      setName('');
+      setShowModal(true);
+      setTimeout(() => setShowModal(false), 2000);
+    }
+  }
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <input
+            type="text"
+            value={name}
+            aria-label="name"
+            onChange={e => setName(e.target.value)}
+            name="name"
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {people.map(p => {
+        return (
+          // <div key={p.id} className="item">
+            <h4 key={p.id}>{p.name}</h4>
+          // </div>
+        )
+      })}
+    </>
+  );
 };
 
 export default Index;
