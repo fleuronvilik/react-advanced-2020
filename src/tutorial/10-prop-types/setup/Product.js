f3b979d1a89947c0640224b635cb31696129b87e
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-17 23:45:02 +0100

    Complete Product and add PropTypes to...
    Prevent cases of missing/undefined props
    Examples: image, price

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+import DEFAULT_IMG from '../../../assets/default-image.jpeg'
+
+const Product = ({image, name, price}) => {
+  return (
+    <article className='product'>
+      <img src={image.url} alt={name}/>
+      <h4>{name}</h4>
+      <p>${price}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  name : PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  price: PropTypes.number//.isRequired,
+};
+Product.defaultProps = {
+  name: "default name",
+  price: 3.99,
+  image: {url: DEFAULT_IMG}
+}
 
 export default Product;
